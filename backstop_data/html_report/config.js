c434cae7c747ad3ec3ff323c744d167ea78ea5de
Brian Han <brhan@indeed.com>
2019-02-01 21:58:40 -0600

    use more consistent url

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_storybook_Button_0_document_0_phone.png",
-        "test": "../bitmaps_test/20190201-215138/backstop_storybook_Button_0_document_0_phone.png",
+        "test": "../bitmaps_test/20190201-215746/backstop_storybook_Button_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_storybook_Button_0_document_0_phone.png",
         "label": "Button",
@@ -26,12 +26,56 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_storybook_Button_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20190201-215138/backstop_storybook_Button_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20190201-215746/backstop_storybook_Button_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "backstop_storybook_Button_0_document_1_tablet.png",
         "label": "Button",
         "misMatchThreshold": 0.1,
         "url": "http://localhost:6006/iframe.html?selectedKind=Button&selectedStory=default",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_storybook_BackstopJS_0_document_0_phone.png",
+        "test": "../bitmaps_test/20190201-215746/backstop_storybook_BackstopJS_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_storybook_BackstopJS_0_document_0_phone.png",
+        "label": "BackstopJS",
+        "misMatchThreshold": 0.1,
+        "url": "https://github.com/garris/BackstopJS#installation",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_storybook_BackstopJS_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20190201-215746/backstop_storybook_BackstopJS_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_storybook_BackstopJS_0_document_1_tablet.png",
+        "label": "BackstopJS",
+        "misMatchThreshold": 0.1,
+        "url": "https://github.com/garris/BackstopJS#installation",
         "expect": 0,
         "viewportLabel": "tablet",
         "diff": {
