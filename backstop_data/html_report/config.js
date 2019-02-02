53f6f3878384c59a9d08200e7b25c87bf0cff52e
Brian Han <brhan@indeed.com>
2019-02-01 22:07:10 -0600

    add docker flag

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -3,52 +3,8 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "../bitmaps_reference/backstop_storybook_Button_0_document_0_phone.png",
-        "test": "../bitmaps_test/20190201-220104/backstop_storybook_Button_0_document_0_phone.png",
-        "selector": "document",
-        "fileName": "backstop_storybook_Button_0_document_0_phone.png",
-        "label": "Button",
-        "misMatchThreshold": 0.1,
-        "url": "http://localhost:6006/iframe.html?selectedKind=Button&selectedStory=default",
-        "expect": 0,
-        "viewportLabel": "phone",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00"
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/backstop_storybook_Button_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20190201-220104/backstop_storybook_Button_0_document_1_tablet.png",
-        "selector": "document",
-        "fileName": "backstop_storybook_Button_0_document_1_tablet.png",
-        "label": "Button",
-        "misMatchThreshold": 0.1,
-        "url": "http://localhost:6006/iframe.html?selectedKind=Button&selectedStory=default",
-        "expect": 0,
-        "viewportLabel": "tablet",
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00"
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
         "reference": "../bitmaps_reference/backstop_storybook_Accordion_Carbon_0_document_0_phone.png",
-        "test": "../bitmaps_test/20190201-220104/backstop_storybook_Accordion_Carbon_0_document_0_phone.png",
+        "test": "../bitmaps_test/20190202-040451/backstop_storybook_Accordion_Carbon_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_storybook_Accordion_Carbon_0_document_0_phone.png",
         "label": "Accordion (Carbon)",
@@ -70,7 +26,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_storybook_Accordion_Carbon_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20190201-220104/backstop_storybook_Accordion_Carbon_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20190202-040451/backstop_storybook_Accordion_Carbon_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "backstop_storybook_Accordion_Carbon_0_document_1_tablet.png",
         "label": "Accordion (Carbon)",
@@ -92,7 +48,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_storybook_BackstopJS_0_document_0_phone.png",
-        "test": "../bitmaps_test/20190201-220104/backstop_storybook_BackstopJS_0_document_0_phone.png",
+        "test": "../bitmaps_test/20190202-040451/backstop_storybook_BackstopJS_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_storybook_BackstopJS_0_document_0_phone.png",
         "label": "BackstopJS",
@@ -114,7 +70,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_storybook_BackstopJS_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20190201-220104/backstop_storybook_BackstopJS_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20190202-040451/backstop_storybook_BackstopJS_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "backstop_storybook_BackstopJS_0_document_1_tablet.png",
         "label": "BackstopJS",
