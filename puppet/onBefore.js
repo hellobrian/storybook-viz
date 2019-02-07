f8f3ab619c3ca6a02823a446c89afeb508696268
Brian Han <brhan@indeed.com>
2019-02-07 12:45:13 -0600

    remove welcome

---
+++ puppet/onBefore.js
@@ -0,0 +1,1 @@
+console.log(`onBefore.js`);
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,15 +5,6 @@ import { action } from "@storybook/addon-actions";
 
 import { Button } from "@storybook/react/demo";
 
-storiesOf("Visual Tests", module).add("reports", () => (
-  <ul>
-    <li>{`${__dirname}/backstop_data/html_report/index.html`}</li>
-    <li>
-      <a href={`${__dirname}/backstop_data/html_report/index.html`}>REPORT</a>
-    </li>
-  </ul>
-));
-
 storiesOf("Button", module)
   .add("default", () => (
     <Button onClick={action("clicked")}>Hello Button</Button>
