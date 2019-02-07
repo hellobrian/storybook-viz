f57085b1915e6a681988347ef478c90c14c20fe0
Brian Han <brhan@indeed.com>
2019-02-07 10:58:51 -0600

    add pokemon

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,6 +5,15 @@ import { action } from "@storybook/addon-actions";
 
 import { Button } from "@storybook/react/demo";
 
+storiesOf("Visual Tests", module).add("reports", () => (
+  <ul>
+    <li>{`${__dirname}/backstop_data/html_report/index.html`}</li>
+    <li>
+      <a href={`${__dirname}/backstop_data/html_report/index.html`}>REPORT</a>
+    </li>
+  </ul>
+));
+
 storiesOf("Button", module)
   .add("default", () => (
     <Button onClick={action("clicked")}>Hello Button</Button>
@@ -16,3 +25,13 @@ storiesOf("Button", module)
       </span>
     </Button>
   ));
+
+const charmander =
+  "https://cdn.bulbagarden.net/upload/thumb/7/73/004Charmander.png/250px-004Charmander.png";
+
+storiesOf("Pokemon", module).add("bulbasaur", () => (
+  <img
+    src="https://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/250px-001Bulbasaur.png"
+    alt="pokemon"
+  />
+));
